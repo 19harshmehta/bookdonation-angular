f808aa24a4cf3adfb5b4b9ab302a4b2a4b3a2dc4
19harshmehta <mehtah631@gmail.com>
2023-09-07 09:22:45 +0530

    'signup'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveSignupComponent } from './reactive-signup/reactive-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveSignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reactive-signup/reactive-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-signup',
+  templateUrl: './reactive-signup.component.html',
+  styleUrls: ['./reactive-signup.component.css']
+})
+export class ReactiveSignupComponent {
+
+}
